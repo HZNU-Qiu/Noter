78e038aedccc1b915d6944ae2a5e04fb2f20dbca
kingjade <18196777756@163.com>
2023-01-02 15:24:46 +0800

    spss

--- docs/.vuepress/config/navbar.js
+++ docs/.vuepress/config/navbar.js
@@ -2,6 +2,7 @@ module.exports = {
     navbar: [
         { text: 'Home', link: '/' },
         { text: 'Java厨', link: '/guide/Java/Demo.md' },
-        { text: '玩点算法', link: '/guide/Algorithm/双指针玩转链表.md'}
+        { text: '玩点算法', link: '/guide/Algorithm/双指针玩转链表.md'},
+        { text: '考试位招租', link: '/guide/SPSS/how2use.md'}
     ]
 }
